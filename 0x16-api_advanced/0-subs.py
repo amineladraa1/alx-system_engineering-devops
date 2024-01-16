db65b2b50be01504177c7fdc3f27c3e6302c856c
amineladraa1 <amineladraa67@gmail.com>
2024-01-16 02:17:53 -0800

    task 0: 0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-"""Module for task 0"""
-
+'''
+    this module contains the function number_of_subscribers
+'''
 import requests
-import time
 from sys import argv
 
-def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
 
-    user_agent = {'User-Agent': 'MyPythonApp/0.0.1'}
-    subs_data = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), 
-                                headers=user_agent, allow_redirects=False)
-    
-    time.sleep(1)
+def number_of_subscribers(subreddit):
+    '''
+        returns the number of subscribers for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
     try:
-        subs_data.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
-        return subs_data.json().get('data').get('subscribers')
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+        return url.get('data').get('subscribers')
+    except Exception:
         return 0
 
+
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: python script.py subreddit_name")
-    else:
-        subreddit_name = argv[1]
-        subscribers_count = number_of_subscribers(subreddit_name)
-
-        print(f"The number of subscribers in r/{subreddit_name} is: {subscribers_count}")
-
+    number_of_subscribers(argv[1])
