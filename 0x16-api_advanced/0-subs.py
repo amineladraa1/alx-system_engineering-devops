c2a8268ff5fb41d8324f6066c8007fd0080a9e66
amineladraa1 <amineladraa67@gmail.com>
2024-01-16 01:54:03 -0800

    task 0: 0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,11 @@ def number_of_subscribers(subreddit):
     to the subreddit"""
     import requests
 
-    subs_count = requests.get("https://www.reddit.com/r/{}/about.json"
-                                .format(subreddit),
-                                headres={"User-Agent": "My-User-Agent"},
-                                allow_redirects=False)
-    if subs_count >= 300:
+    subs_call = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if subs_call.status_code >= 300:
         return 0
 
-    return subs_count.json().get("data").get("subscribers")
+    return subs_call.json().get("data").get("subscribers")
